f5bb2ea9176fca2ee58af6b4b8ee5665ed517c03
welingtondev99 <welingtondev99@gmail.com>
2026-08-17 00:21:40 -0300

    add contato

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // mudando cor do botão enviar
-let btnProjetos = document.querySelector(".but-projetos")
-let circulo = document.querySelector(".circulo")
+const btnProjetos = document.querySelector(".but-projetos")
+const btnComigo = document.querySelector(".but-comigo")
+const circulo = document.querySelector(".circulo")
+const projetos = document.querySelector("#projetos") 
+const contato = document.querySelector("#contato") 
 
 btnProjetos.addEventListener('mouseenter', () =>{
     btnProjetos.style.transition = '0.2s'
@@ -12,3 +15,43 @@ btnProjetos.addEventListener('mouseleave', () => {
     btnProjetos.style.backgroundColor = ''
     circulo.style.backgroundColor = ''
 });
+
+
+// levando tela até a parte dos projetos
+btnProjetos.addEventListener('click',function(){
+    projetos.scrollIntoView({
+        behavior: "smooth"
+    })
+})
+
+// levando tela até a parte do contato
+btnComigo.addEventListener('click',function(){
+    contato.scrollIntoView({
+        behavior: "smooth"
+    })
+    console.log('ola')
+})
+
+//dando vida ao card dos projetos
+const cards = document.querySelectorAll(".card-projetos")
+
+cards.forEach(card=>{
+    card.addEventListener('mouseenter', ()=>{
+        card.style.color ='#3b82f6'
+        card.children[3].children[0].style.backgroundColor = '#0f172a'
+        card.children[3].children[0].style.fill = 'white';
+        card.children[3].children[0].style.transition = '0.15s'
+        card.style.transition = '0.15s'
+    })
+});
+
+cards.forEach(card=>{
+    card.addEventListener('mouseleave', ()=>{
+        card.style.color =''
+        const img = card.lastElementChild
+        card.children[3].children[0].style.backgroundColor = ''
+        card.children[3].children[0].style.fill = '';
+
+    })
+});
+

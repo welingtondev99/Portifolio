4346741ca2cedbf5b194b318798ee5c4c563ced4
welingtondev99 <welingtondev99@gmail.com>
2026-08-18 23:56:21 -0300

    menu navegador

--- js/main.js
+++ js/main.js
@@ -1,6 +1,52 @@
 // import { arreyProjeto } from "./dados.js";
 // import { create } from "./projetos.js";
 
+//criando menu dispositivo movel
+const menu = document.querySelector(".menu")
+const imgMemu = document.querySelector(".img-menu")
+const itensMenu = document.querySelector(".alinhamento-nave")
+const divMenu = document.querySelector(".t")
+
+menu.addEventListener("click", ()=>{
+    if (imgMemu.classList.contains("aberto")){
+        imgMemu.classList.remove('aberto')
+        imgMemu.classList.add('fechado')
+        divMenu.innerHTML = 
+            `  <ul class="alinhamento-nave-menu">
+                    <li><a href="#inicio">Início</a></li>
+                    <li><a href="#sobre">Sobre mim</a></li>
+                    <li><a href="#habilidades">Habilidades</a></li>
+                    <li><a href="#projetos">Projetos</a></li>
+                    <li><a href="#contato">Contato</a></li>
+                </ul>
+            `
+        divMenu.style.display = 'flex'
+        }
+
+    else{
+        imgMemu.classList.remove('fechado')
+        imgMemu.classList.add('aberto')
+        
+        divMenu.innerHTML = ''
+    }
+})
+//fechando menu ao clicar em um botao(link)
+divMenu.addEventListener('click', (evento)=>{
+    if(evento.target.tagName === 'A'){
+        divMenu.style.display = 'none'
+        imgMemu.classList.add('aberto')
+        imgMemu.classList.remove('fechado')
+    }
+})
+
+//removendo bug de menu aberto ao esticar tela
+if(itensMenu.style.display === 'flex'){
+    console.log('click 1')
+    divMenu.innerHTML = ''
+}
+
+
+
 // mudando cor do botão enviar
 const btnProjetos = document.querySelector(".but-projetos")
 const btnComigo = document.querySelector(".but-comigo")
